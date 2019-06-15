4d10a14224a475aed00b9c1e6f61ce3232e2b5a7
Unity Technologies <@unity.com>
2019-06-15 06:09:58 +0200

    com.unity.ugui@1.0.0 in Unity 2019.2.0b7 ## [1.0.0] - 2019-01-08 This is the first release of Unity UI as a built in package.

--- Runtime/UI/Core/ToggleGroup.cs
+++ Runtime/UI/Core/ToggleGroup.cs
@@ -31,6 +31,16 @@ namespace UnityEngine.UI
         protected ToggleGroup()
         {}
 
+        /// <summary>
+        /// Because all the Toggles have registered themselves in the OnEnabled, Start should check to
+        /// make sure at least one Toggle is active in groups that do not AllowSwitchOff
+        /// </summary>
+        protected override void Start()
+        {
+            EnsureValidState();
+            base.Start();
+        }
+
         private void ValidateToggleIsInGroup(Toggle toggle)
         {
             if (toggle == null || !m_Toggles.Contains(toggle))
@@ -66,12 +76,6 @@ namespace UnityEngine.UI
         {
             if (m_Toggles.Contains(toggle))
                 m_Toggles.Remove(toggle);
-
-            if (!allowSwitchOff && !AnyTogglesOn() && m_Toggles.Count != 0)
-            {
-                m_Toggles[0].isOn = true;
-                NotifyToggleOn(m_Toggles[0]);
-            }
         }
 
         /// <summary>
@@ -82,11 +86,18 @@ namespace UnityEngine.UI
         {
             if (!m_Toggles.Contains(toggle))
                 m_Toggles.Add(toggle);
+        }
 
-            if (!allowSwitchOff && !AnyTogglesOn())
+        /// <summary>
+        /// Ensure that the toggle group still has a valid state. This is only relevant when a ToggleGroup is Started
+        /// or a Toggle has been deleted from the group.
+        /// </summary>
+        public void EnsureValidState()
+        {
+            if (!allowSwitchOff && !AnyTogglesOn() && m_Toggles.Count != 0)
             {
-                toggle.isOn = true;
-                NotifyToggleOn(toggle);
+                m_Toggles[0].isOn = true;
+                NotifyToggleOn(m_Toggles[0]);
             }
         }
 
