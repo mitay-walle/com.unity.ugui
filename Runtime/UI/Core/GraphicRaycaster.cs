7ab47bb142b8fd76a522f102c2416b9b65de8176
Unity Technologies <@unity.com>
2019-10-09 09:06:22 +0200

    com.unity.ugui@1.0.0 in Unity 2019.3.0b7 ## [1.0.0] - 2019-01-08 This is the first release of Unity UI as a built in package.

--- Runtime/UI/Core/GraphicRaycaster.cs
+++ Runtime/UI/Core/GraphicRaycaster.cs
@@ -272,6 +272,7 @@ namespace UnityEngine.UI
                         module = this,
                         distance = distance,
                         screenPosition = eventPosition,
+                        displayIndex = displayIndex,
                         index = resultAppendList.Count,
                         depth = m_RaycastResults[index].depth,
                         sortingLayer = canvas.sortingLayerID,
